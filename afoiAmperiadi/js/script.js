095e39f2bed19eb453882edf164c4b9ef7d5d41d
Dimitra <dimitraamp.1997@gmail.com>
2020-05-07 23:35:43 +0300

    My first website

---
+++ afoiAmperiadi/js/script.js
@@ -0,0 +1,19 @@
+$(function(){
+
+
+	 $('.navbar-collapse a').click(function(){
+            $(".navbar-collapse").collapse('hide');
+        });
+
+	$("#link2").click(function(){
+	 document.getElementById("box").style.paddingTop = "100px";
+	 });
+
+	$("#link3").click(function(){
+		document.getElementById("section2").style.paddingTop="150px";
+	});
+	 
+	});
+
+
+
