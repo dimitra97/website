2f72dda283c697f9c6399cab67b354c98552e54e
Dimitra <dimitraamp.1997@gmail.com>
2020-05-11 19:47:18 +0300

    My Website

--- mywebsite/js/script.js
+++ mywebsite/js/script.js
@@ -1,18 +1,14 @@
 $(function(){
 
 
-	 $('.navbar-collapse a').click(function(){
-            $(".navbar-collapse").collapse('hide');
+	 $("#physics-link").click(function(){
+           document.getElementById("section2").style.display = "block";
+        });
+	 $("#physics-link").blur(function(){
+           document.getElementById("section2").style.display = "none";
         });
 
-	$("#link2").click(function(){
-	 document.getElementById("box").style.paddingTop = "100px";
-	 });
-
-	$("#link3").click(function(){
-		document.getElementById("section2").style.paddingTop="120px";
-	});
-	 
+	
 	});
 
 
