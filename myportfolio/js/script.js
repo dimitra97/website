d0d4c2d46f75580cff8f36e67af79e61ffd2d87b
Dimitra <dimitraamp.1997@gmail.com>
2020-05-17 17:44:44 +0300

    Update

--- myportfolio/js/script.js
+++ myportfolio/js/script.js
@@ -1,75 +1,26 @@
 
 $(function(){
 
+var h = window.getComputedStyle(
+	document.querySelector('.home-font'), ':after'
+).getPropertyValue('height')
+console.log(h);
+	document.getElementById("section1").style.top=h;
 
-	 $("#physics-link").click(function(){
-           var x=document.getElementById("section2");
-           var y=document.getElementById("section3");
-           var z=document.getElementById("section4");
-           y.style.display="";
-           z.style.display="";
-           document.getElementById("maths-link").style.backgroundColor="";
-           document.getElementById("home-page").style.backgroundColor="";
-           document.getElementById("contact-link").style.backgroundColor="";
-           if(x.style.display==""){
-           	x.style.display="block";
-           	document.getElementById("physics-link").style.backgroundColor="#FFEFD5";
-           }else{
-           	x.style.display="";
-           }
-        });
+var header=document.getElementById("myHeader");
+	$("li>a").click(function(){
+		header.style.backgroundColor="black";
+	});
 
-	 $("#maths-link").click(function(){
-	 	var x=document.getElementById("section3");
-	 	var y=document.getElementById("section2");
-	 	var z=document.getElementById("section4");
-	 	z.style.display="";
-	 	y.style.display="";
-	 	document.getElementById("physics-link").style.backgroundColor="";
-	 	document.getElementById("home-page").style.backgroundColor="";
-	 	document.getElementById("contact-link").style.backgroundColor="";
-	 	if(x.style.display==""){
-           	x.style.display="block";
-           	document.getElementById("maths-link").style.backgroundColor="#FFEFD5";
-           }else{
-           	x.style.display="";
-           }
+	
+	$(document).scroll(function(){
+   if($(this).scrollTop() > 0) {   
+        header.style.backgroundColor="black";
+    } else  { 
+   		header.style.backgroundColor="inherit";
+     };
+});
 
-
-	 });
-
-	  $("#contact-link").click(function(){
-	 	var x=document.getElementById("section4");
-	 	var y=document.getElementById("section2");
-	 	var z=document.getElementById("section3");
-	 	y.style.display="";
-	 	z.style.display="";
-	 	document.getElementById("physics-link").style.backgroundColor="";
-	 	document.getElementById("maths-link").style.backgroundColor="";
-	 	document.getElementById("home-page").style.backgroundColor="";
-	 	if(x.style.display==""){
-           	x.style.display="block";
-           	document.getElementById("contact-link").style.backgroundColor="#FFEFD5";
-           }else{
-           	x.style.display="";
-           }
-
-
-	 });
-
-	 $("#home-page").click(function(){
-	 	document.getElementById("home-page").style.backgroundColor="#FFEFD5";
-	 	document.getElementById("physics-link").style.backgroundColor="";
-	 	document.getElementById("maths-link").style.backgroundColor="";
-	 	document.getElementById("contact-link").style.backgroundColor="";
-	 	var x=document.getElementById("section2");
-	 	var y=document.getElementById("section3");
-	 	var z=document.getElementById("section4");
-	 	x.style.display="";
-	 	y.style.display="";
-	 	z.style.display="";
-
-	 });
 });
 	
 
