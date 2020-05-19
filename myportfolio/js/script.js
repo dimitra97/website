9c8d3a1076f5a496f5ec8ad0629bf3b0ee30e0a7
Dimitra <dimitraamp.1997@gmail.com>
2020-05-19 22:22:00 +0300

    Update

--- myportfolio/js/script.js
+++ myportfolio/js/script.js
@@ -4,14 +4,28 @@ $(function(){
 var h = window.getComputedStyle(
 	document.querySelector('.home-font'), ':after'
 ).getPropertyValue('height')
-console.log(h);
 	document.getElementById("section1").style.top=h;
 
+	var sect1=document.getElementById("section1");
+	var top2= 2*sect1.offsetTop;
+	var top3=3*sect1.offsetTop;
+	document.getElementById("section2").style.top=top2+"px";
+	document.getElementById("section3").style.top=top3+"px";
+
+
+
 var header=document.getElementById("myHeader");
+
 	$("li>a").click(function(){
 		header.style.backgroundColor="black";
 	});
 
+	var sec1= document.getElementById("section1");
+	var w=window.innerWidth;
+
+	if (w>1300){
+		$("#about-me-text").style.fontSize="28px";
+	}
 	
 	$(document).scroll(function(){
    if($(this).scrollTop() > 0) {   
